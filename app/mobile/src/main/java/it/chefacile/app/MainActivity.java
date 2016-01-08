b87860d4aacad8de7dc7446d54a2ff28e91afba6
fedmeloni <fed.meloni@gmail.com>
2016-01-08 15:39:27 +0100

    Various changes

--- app/mobile/src/main/java/it/chefacile/app/MainActivity.java
+++ app/mobile/src/main/java/it/chefacile/app/MainActivity.java
@@ -5,80 +5,122 @@ import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailText;
+    TextView responseView;
+    ProgressBar progressBar;
+
+    String API_ID = "c098d0fb";
+    String API_KEYS = "90b990034f035755978a14d3bc8a72ec";
+    String URLProva = "http://api.yummly.com/v1/api/recipes?_app_id=" + API_ID + "&_app_key=" + API_KEYS + "&allowedIngredient[]=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
-        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
-        actionA.setSize(FloatingActionButton.SIZE_MINI);
-        actionA.setOnClickListener(new View.OnClickListener() {
+        responseView = (TextView) findViewById(R.id.responseView);
+        emailText = (EditText) findViewById(R.id.emailText);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        final FloatingActionButton actionABC = (FloatingActionButton) findViewById(R.id.action_abc);
+        actionABC.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), SearchActivity.class);
-                startActivityForResult(myIntent, 0);
-
-               // Snackbar.make(view, "Non disponibile, mangia l'aria", Snackbar.LENGTH_LONG)
-               //         .setAction("Action", null).show();
-
+            public void onClick(View v) {
+                new RetrieveFeedTask().execute();
             }
-        });
 
+            // Snackbar.make(view, "Non disponibile, mangia l'aria", Snackbar.LENGTH_LONG)
+            //         .setAction("Action", null).show();
 
-        final FloatingActionButton actionB = (FloatingActionButton) findViewById(R.id.action_b);
-        actionB.setSize(FloatingActionButton.SIZE_MINI);
-        actionB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                    Intent myIntent = new Intent(view.getContext(), PreferredActivity.class);
-                    startActivityForResult(myIntent, 0);
-
-               // Snackbar.make(view, "Non disponibile", Snackbar.LENGTH_LONG)
-               //         .setAction("Action", null).show();
-            }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        private Exception exception;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("");
         }
 
-        return super.onOptionsItemSelected(item);
+        protected String doInBackground(Void... urls) {
+            String email = emailText.getText().toString();
+            // Do some validation here about String email
+
+            try {
+                URL url = new URL(URLProva + email);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            responseView.setText(response);
+            //  check this.exception
+            //  do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+        }
     }
+
 
 
 }
