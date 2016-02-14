171afaa383a94b5644777f2c134e64d8a84896ff
Gaia Beatrice <gaiabeatrice@gmail.com>
2016-02-14 15:39:36 +0100

    Added  ingredients list

--- app/mobile/src/main/java/it/chefacile/app/MainActivity.java
+++ app/mobile/src/main/java/it/chefacile/app/MainActivity.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private Context mContext;
@@ -60,14 +61,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         mListView = (MaterialListView) findViewById(R.id.material_listview);
-        try {
-            fillArray();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-
 
         TutorialButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -79,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
         AddButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
                 if(editText.getText().toString().equals("")){
                     ingredients += editText.getText().toString().trim() + "";
                     editText.getText().clear();
@@ -86,25 +82,35 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else {
                     ingredients += editText.getText().toString().trim() + ",";
+                    String singleIngredient = editText.getText().toString().trim();
+                    adapter.add(singleIngredient.substring(0,1).toUpperCase() + singleIngredient.substring(1));
                     editText.getText().clear();
+                    Log.d("INGREDIENTS ,", ingredients);
                 }
-                responseView.setText(ingredients);
+                //responseView.setText(ingredients);
                 //ingredients += editText.getText().toString().trim() + ",";
 
             }
         });
+
+        try {
+            fillArray();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         responseView = (TextView) findViewById(R.id.responseView);
         editText = (EditText) findViewById(R.id.ingredientText);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         final FloatingActionButton actionABC = (FloatingActionButton) findViewById(R.id.action_abc);
+        actionABC.bringToFront();
         actionABC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-                        new RetrieveFeedTask().execute();
+                new RetrieveFeedTask().execute();
 
 
             }
@@ -122,20 +128,20 @@ public class MainActivity extends AppCompatActivity {
         protected void onPreExecute() {
             Log.d("Tect",responseView.getText().toString());
             progressBar.setVisibility(View.VISIBLE);
-           // responseView.setText("");
+            // responseView.setText("");
         }
 
         protected String doInBackground(Void... urls) {
-            String ingredient = responseView.getText().toString();
+            //String ingredient = responseView.getText().toString();
 
             // Do some validation here about String ingredient
 
             try {
 
-                URL urlSpoo = new URL(urlSpo + ingredient + "&number=20");
+                URL urlSpoo = new URL(urlSpo + ingredients + "&number=20&ranking=1");
                 HttpURLConnection urlConnection = (HttpURLConnection) urlSpoo.openConnection();
                 //TODO: Changing key values
-                urlConnection.setRequestProperty("KEY", "KEY");
+                urlConnection.setRequestProperty("KEY","KEY");
 
 
                 try {
@@ -158,17 +164,21 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         protected void onPostExecute(String response) {
+
             if(response == null) {
                 response = "THERE WAS AN ERROR";
                 Snackbar.make(responseView, "Network connectivity unavailable", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 progressBar.setVisibility(View.GONE);
             }
+            else if(response.toString().trim().equals("[]") || response.toString().trim().equals("")){
+                Snackbar.make(responseView, "No recipes for these ingredients", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                progressBar.setVisibility(View.GONE);
+            }
             else {
                 progressBar.setVisibility(View.GONE);
-                Log.i("INFO", response);
-               // responseView.setText(response);
-
+                // responseView.setText(response);
                 Intent myIntent1 = new Intent(MainActivity.this, ResultsActivity.class);
                 myIntent1.putExtra("mytext", response);
                 startActivity(myIntent1);
@@ -197,8 +207,6 @@ public class MainActivity extends AppCompatActivity {
         cards.add(generateNewCard());
         mListView.getAdapter().addAll(cards);
     }
-
-
 
     private Card generateNewCard() {
         return new Card.Builder(this)
